7c16f3a6b0c646fb4269d1618e768a9387d1b170
Leah Brenny <leahbrenny@Leahs-MacBook-Pro.local>
2022-11-07 16:20:02 -0600

    switching to writing in jsx and created a Pet component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,15 @@
-import React from "react";
-import { ReactDOM } from "react-dom";
-
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
-};
+import { createRoot } from "react-dom/client";
+import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      animal: "Cat",
-      name: "Fern",
-      breed: "Tortishell",
-    }),
-    React.createElement(Pet, {
-      animal: "Cat",
-      name: "Clover",
-      breed: "Dilute Calico",
-    }),
-    React.createElement(Pet, {
-      animal: "Cat",
-      name: "Ivy",
-      breed: "Dilute Tortishell",
-    }),
-  ]);
-};
+  <div>
+    <h1>Adopt Me!</h1>
+    <Pet name="Fern" animal="Cat" breed="Tortishell" />
+    <Pet name="Clover" animal="Cat" breed="Dilute Calico" />
+    <Pet name="Ivy" animal="Cat" breed="Dilute Tortishell" />
+  </div>
+}
 
 const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
-root.render(React.createElement(App));
+const root = createRoot(container);
+root.render(<App/>);
