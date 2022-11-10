8124ed10aa62b30f32969e401fc4d408acb0c282
Leah Brenny <leahbrenny@Leahs-MacBook-Pro.local>
2022-11-10 17:11:41 -0600

    10-uncontrolled-forms

--- src/App.jsx
+++ src/App.jsx
@@ -9,21 +9,21 @@ const queryClient = new QueryClient({
     queries: {
       staleTime: Infinity,
       cacheTime: Infinity,
-    }
-  }
-})
+    },
+  },
+});
 
 const App = () => {
   return (
     <BrowserRouter>
-    <QueryClientProvider client={queryClient}>
-      <header>
-        <Link to="/"> Adopt Me! </Link>
-      </header>
-      <Routes>
-        <Route path="/details/:id" element={<Details />} />
-        <Route path="/" element={<SearchParams />} />
-      </Routes>
+      <QueryClientProvider client={queryClient}>
+        <header>
+          <Link to="/"> Adopt Me! </Link>
+        </header>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
       </QueryClientProvider>
     </BrowserRouter>
   );
--- src/Details.jsx
+++ src/Details.jsx
@@ -7,31 +7,31 @@ const Details = () => {
   const results = useQuery(["details", id], fetchPet);
 
   if (results.isError) {
-    return (
-        <h1>Error</h1>
-    )
+    return <h1>Error</h1>;
   }
 
   if (results.isLoading) {
     return (
-        <div className="loading-pane">
-            <h2 className="loader">🦴</h2>
-        </div>
-    )
+      <div className="loading-pane">
+        <h2 className="loader">🦴</h2>
+      </div>
+    );
   }
 
-const pet = results.data.pets[0];
+  const pet = results.data.pets[0];
 
   return (
     <div className="details">
-        <div>
-            <h1>{pet.name}</h1>
-            <h2>{pet.animal} - {pet.breed} - {pet.city}, {pet.state}</h2>
-            <button>Adopt {pet.name}</button>
-            <p>{pet.description}</p>
-        </div>
+      <div>
+        <h1>{pet.name}</h1>
+        <h2>
+          {pet.animal} - {pet.breed} - {pet.city}, {pet.state}
+        </h2>
+        <button>Adopt {pet.name}</button>
+        <p>{pet.description}</p>
+      </div>
     </div>
-  )
+  );
 };
 
 export default Details;
